e3b9cf639add3b4fdcb288c8653f34f38a544173
Indranil Halder <indranilhalder.dev@gmail.com>
2026-02-11 13:26:52 +0530

    feat: add turborepo
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -5,11 +5,11 @@ export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
+        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
         <div className="flex gap-2">
           <Button>Button</Button>
           <Button variant="outline">Outline</Button>
-          <p>{add(2, 2)}</p>
+          <p>{add(3, 2)}</p>
         </div>
       </div>
     </div>
